1dd690ac65fff320ce1cf2c6a79928d53b4e465d
julio.cesar <julio.cesar@soaexpert.com.br>
2020-02-15 22:05:43 -0300

    New page about and refactoring

---
+++ src/components/Profile/index.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Profile = () => (
+  <div className="Profile-wrapper">
+    <h1>Julio Cesar</h1>
+    <h2>Programador Frontend</h2>
+    <p>Minha descricao aqui ...</p>
+  </div>
+)
+
+export default Profile
