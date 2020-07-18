8afc338fa193c393826e4653cc60b0bd2dd3ae83
jcc0xp <julioccolv@gmail.com>
2020-07-18 12:37:11 -0300

    Refatoracao para enviar dados via props (PostItem Component)

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,18 +7,14 @@ import PostItem from "../components/PostItem";
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <PostItem />
-    <PostItem />
-    <PostItem />
-    <PostItem />
-    <PostItem />
-    <PostItem />
-    <PostItem />
-    <PostItem />
-    <PostItem />
-    <PostItem />
-    <PostItem />
-    <PostItem />
+    <PostItem
+      slug="/about/"
+      category="Misc"
+      date="30 de Julho de 2019"
+      timeToRead="5"
+      title="Diga nao ao Medium: tenha sua propria plataforma"
+      description="Algumas razoes para voce ter sua propria plataforma ao inves de solucoes como o Medium"
+    />
   </Layout>
 )
 
