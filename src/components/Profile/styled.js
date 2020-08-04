1644e5e5ac96f8cdbe58b7e38e2b33ce4bd62ebb
jcc0xp <julioccolv@gmail.com>
2020-08-03 22:47:07 -0300

    Add responsive styles to all components

--- src/components/Profile/styled.js
+++ src/components/Profile/styled.js
@@ -1,8 +1,9 @@
 import styled from "styled-components"
+import media from "styled-media-query"
 import { Link } from "gatsby"
 
 export const ProfileWrapper = styled.section`
-    color: #889a6;
+    color: var(--texts);
     display: flex;
     flex-direction: column;
 `
@@ -12,6 +13,11 @@ export const ProfileLink = styled(Link)`
     text-decoration: none;
     transition: color 0.5s;
 
+    ${media.lessThan("large")`
+        display: flex;
+        text-align: left;
+    `}
+
     &:hover {
         color: var(--highlight);
     }
@@ -20,12 +26,22 @@ export const ProfileLink = styled(Link)`
 export const ProfileAuthor = styled.h1`
     font-size: 1.6rem;
     margin: 0.5rem auto 1.5rem;
+
+    ${media.lessThan("large")`
+        font-size: 1.2rem;
+        margin: 0 0 0 10px;
+    `}
 `
 
 export const ProfilePosition = styled.small`
     display: block;
     font-size: 1.2rem;
     font-weight: 300;
+
+    ${media.lessThan("large")`
+        font-size: 0.8rem;
+        margin-top: 0.2rem;
+    `}
 `
 
 export const ProfileDescription = styled.p`
@@ -33,4 +49,8 @@ export const ProfileDescription = styled.p`
     font-size: 1rem;
     font-weight: 300;
     line-height: 1.4;
+
+    ${media.lessThan("large")`
+        display: none;
+    `}
 `
